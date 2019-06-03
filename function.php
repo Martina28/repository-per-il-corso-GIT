68b4d0a824ca250fac7fc7dab907b5e539ab2b9b
Martina Casciotti <martina.casciotti@hotmail.com>
2019-06-03 10:17:03 -0700

    Aggiunta fuzione di divisione con controllo

--- function.php
+++ function.php
@@ -14,4 +14,13 @@ function sottrazione (n1, n2){
 function moltiplicazione (n1, n2){
     return n1*n2;
     }
+	
+	/*divisione*/
+	
+function divisione (n1, n2){
+	
+	if {n2!==0}
+	{return n1/n2;}
+	else {return "error"}
+    }
 ?>
