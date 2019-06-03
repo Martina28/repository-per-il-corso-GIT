464b46840413657f696c2fa477ed853a69424a09
Martina Casciotti <martina.casciotti@hotmail.com>
2019-06-03 09:49:54 -0700

    Aggiunta funzione di moltiplicazione

--- function.php
+++ function.php
@@ -4,9 +4,14 @@
 function addizione (n1, n2){
     return n1+n2;
     }
-	*sottrazione*/
+	/*sottrazione*/
 
 function sottrazione (n1, n2){
-    return n1-n2;
+    return n1-n2;    }
+	
+	/*moltiplicazione*/
+	
+function moltiplicazione (n1, n2){
+    return n1*n2;
     }
 ?>
